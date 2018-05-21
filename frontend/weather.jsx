ce94e4f4bf4d36a8c272f14ec3ab337f1ddf639b
David Harris <dahveed15@gmail.com>
2018-05-21 16:50:30 -0700

    get location of device

--- frontend/weather.jsx
+++ frontend/weather.jsx
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom';
 
 class Weather extends React.Component {
 
-  
+  componentdidMount() {
+
+    let options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition(this.success, this.error, options);
+  }
+
+  success(pos) {
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
 
   render() {
     return (
